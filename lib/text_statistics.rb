90663d8ac332229f7abf2647155a68d946d1ef4b
Ярослав Мирхаликов <42945722+Mirkhalikov@users.noreply.github.com>
2024-05-04 15:51:28 +0000

    Написал класс TextStatistics и добавил в него методы для подсчёта статистики

---
+++ lib/text_statistics.rb
@@ -0,0 +1,32 @@
+class TextStatistics
+  attr_reader :text
+
+  def initialize(text)
+    @text = text
+  end
+
+  def word_count
+    text.split.size
+  end
+
+  def sentence_count
+    text.split(/\.|\?|!/).size
+  end
+
+  def unique_words_count
+    text.downcase.split.uniq.size
+  end
+
+  def average_sentence_length
+    words = text.split
+    sentences = text.split(/\.|\?|!/)
+    words.size.to_f / sentences.size
+  end
+
+  def word_frequency
+    words = text.downcase.split
+    frequency = Hash.new(0)
+    words.each { |word| frequency[word] += 1 }
+    frequency
+  end
+end

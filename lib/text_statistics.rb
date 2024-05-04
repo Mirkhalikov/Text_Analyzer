d903acd282bf2886a50400ada607da2d8a9dbf2b
Ярослав Мирхаликов <42945722+Mirkhalikov@users.noreply.github.com>
2024-05-04 17:45:54 +0000

    Добавил методы для высчитывания средней длины слова,  индекса уникальности текста и частоты использования символов и предлогов(союзов)

--- lib/text_statistics.rb
+++ lib/text_statistics.rb
@@ -31,4 +31,30 @@ class TextStatistics
     words.each { |word| frequency[word] += 1 }
     frequency
   end
+
+  def average_word_length
+    words = text.split
+    total_length = words.map(&:length).reduce(:+)
+    total_length.to_f / words.size
+  end
+
+  def character_frequency
+    frequency = Hash.new(0)
+    text.chars.each { |char| frequency[char] += 1 unless char.match(/\s/) }
+    frequency
+  end
+
+  def prepositions_and_conjunctions_frequency
+    prepositions_and_conjunctions = %w[in on at by for from with to into onto upon as about after before during since through]
+    words = text.downcase.split
+    frequency = Hash.new(0)
+    words.each { |word| frequency[word] += 1 if prepositions_and_conjunctions.include?(word) }
+    frequency
+  end
+
+  def text_uniqueness_index
+    unique_words = text.downcase.split.uniq.size
+    total_words = text.split.size
+    (unique_words.to_f / total_words) * 100
+  end
 end
